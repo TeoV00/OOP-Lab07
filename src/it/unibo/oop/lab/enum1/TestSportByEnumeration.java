2f0b1b3364815ec1543c4bb1fee563783b6de8ef
Matteo <matteo.violani@studio.unibo.it>
2020-11-10 17:55:46 +0100

    test of class

--- src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
+++ src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.lab.enum1;
 
+import it.unibo.oop.lab.socialnetwork.User;
+
 /**
  * This is going to act as a test for
  * {@link it.unibo.oop.lab.enum1.SportSocialNetworkUserImpl}
@@ -24,6 +26,13 @@ public final class TestSportByEnumeration {
          * 
          * By now, you know how to do it
          */
-        // TODO
+        
+        SportSocialNetworkUserImpl<User> puletti = new SportSocialNetworkUserImpl<>("Pippo", "Paoletti", "puletti",43);
+        puletti.addSport(Sport.BASKET);
+        puletti.addSport(Sport.VOLLEY);
+        puletti.addSport(Sport.F1);
+        System.out.println("Puletti DONT LIKE motogp: " + !puletti.hasSport(Sport.MOTOGP));
+        System.out.println("Puletti Like Basket: " + puletti.hasSport(Sport.BASKET));
+        
     }
 }
