0209665e1c861d361d82846b296042e31041c66e
Matteo <matteo.violani@studio.unibo.it>
2020-11-21 13:15:23 +0100

    impl. getter method sport2Social

--- src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
+++ src/it/unibo/oop/lab/enum2/Sport2SocialNetworkUserImpl.java
@@ -5,6 +5,8 @@ package it.unibo.oop.lab.enum2;
 
 import it.unibo.oop.lab.socialnetwork.SocialNetworkUserImpl;
 import it.unibo.oop.lab.socialnetwork.User;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * 
@@ -26,11 +28,9 @@ import it.unibo.oop.lab.socialnetwork.User;
 public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUserImpl<U> {
 
     /*
-     * TODO
-     * 
      * add a field to keep track of the set of sports followed/done by a user
      */
-
+    private final Set<Sport> followedSport = new TreeSet<>();
     /**
      * Builds a new {@link Sport2SocialNetworkUserImpl}.
      * 
@@ -75,7 +75,7 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      *            a sport followed/done by the user
      */
     public void addSport(final Sport sport) {
-
+        followedSport.add(sport);
     }
 
     /**
@@ -87,7 +87,7 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * @return true if user likes sport s
      */
     public boolean likesSport(final Sport s) {
-        return false;
+        return followedSport.contains(s);
     }
 
     /*
@@ -101,10 +101,19 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * @return the set of individual sport this user practices/follows
      */
     /*
-     * public Set<Sport> getIndividualSports() { return null; }
-     * 
-     * 
-     * /** Returns the set of sports which are practiced in a given place.
+     * That could be resolved with lambda and stream
+     * */
+    public Set<Sport> getIndividualSports() {
+        Set<Sport> indSports = new TreeSet<>();
+        for(Sport sport : this.followedSport) {
+            if (sport.isIndividualSport()) {
+                indSports.add(sport);
+            }
+        }
+        return indSports; 
+        }
+    /** 
+     * Returns the set of sports which are practiced in a given place.
      * 
      * @param p the place in which the sport is practiced in order to be
      * included in the resulting set
@@ -112,6 +121,16 @@ public class Sport2SocialNetworkUserImpl<U extends User> extends SocialNetworkUs
      * @return the set of sport practiced in a given place
      */
     /*
-     * public Set<Sport> getSportPracticedInPlace(Place p) { return null; }
-     */
+     * That could be resolved with lambda and stream
+     * */
+    public Set<Sport> getSportPracticedInPlace(Place p) {
+        Set<Sport> inPlaceSports = new TreeSet<>();
+        for(Sport sport : this.followedSport) {
+            if (sport.getPlace().equals(p)) {
+                inPlaceSports.add(sport);
+            }
+        }
+        return inPlaceSports; 
+        }
+     
 }
